867cc2a2da9b07d07042d2dbec13ea1eb456afdc
Lurio <bruno.stier@uha.fr>
2024-12-02 14:15:51 +0100

    Code update

--- bezier_and_bspline.py
+++ bezier_and_bspline.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import BSpline
 
+# Fonction pour générer une courbe de Bézier
 def bezier_curve(control_points, num_points=100):
     """
     Génère une courbe de Bézier à partir des points de contrôle.
@@ -13,17 +15,7 @@ def bezier_curve(control_points, num_points=100):
     t_values = np.linspace(0, 1, num_points)
     return np.array([de_casteljau(control_points, t) for t in t_values])
 
-# Exemple d'utilisation
-control_points = np.array([[0, 0], [1, 2], [3, 3], [4, 0]])
-curve = bezier_curve(control_points)
-
-plt.plot(curve[:, 0], curve[:, 1], label="Courbe de Bézier")
-plt.scatter(control_points[:, 0], control_points[:, 1], color='red', label="Points de contrôle")
-plt.legend()
-plt.show()
-
-from scipy.interpolate import BSpline
-
+# Fonction pour générer une courbe B-spline
 def b_spline_curve(control_points, degree, num_points=100):
     """
     Génère une courbe B-spline à partir des points de contrôle.
@@ -34,12 +26,31 @@ def b_spline_curve(control_points, degree, num_points=100):
     spline = BSpline(knots, control_points, degree)
     return spline(t)
 
-# Exemple d'utilisation
-control_points = np.array([[0, 0], [1, 2], [3, 3], [4, 0]])
-degree = 3  # Degré de la B-spline
-curve = b_spline_curve(control_points, degree)
+# Points de contrôle plus intéressants
+control_points_bezier = np.array([[0, 0], [2, 3], [4, 7], [6, 5], [8, -2], [10, 2]])
+control_points_bspline = np.array([[0, 0], [2, 3], [4, 7], [6, 5], [8, -2], [10, 2]])
 
-plt.plot(curve[:, 0], curve[:, 1], label="Courbe B-spline")
-plt.scatter(control_points[:, 0], control_points[:, 1], color='red', label="Points de contrôle")
+# Génération des courbes
+curve_bezier = bezier_curve(control_points_bezier, num_points=200)
+degree_bspline = 3
+curve_bspline = b_spline_curve(control_points_bspline, degree_bspline, num_points=200)
+
+# Visualisation des résultats
+plt.figure(figsize=(12, 6))
+
+# Courbe de Bézier
+plt.subplot(1, 2, 1)
+plt.plot(curve_bezier[:, 0], curve_bezier[:, 1], label="Courbe de Bézier", color="blue")
+plt.scatter(control_points_bezier[:, 0], control_points_bezier[:, 1], color='red', label="Points de contrôle")
+plt.title("Courbe de Bézier")
 plt.legend()
+
+# Courbe B-spline
+plt.subplot(1, 2, 2)
+plt.plot(curve_bspline[:, 0], curve_bspline[:, 1], label="Courbe B-spline", color="green")
+plt.scatter(control_points_bspline[:, 0], control_points_bspline[:, 1], color='red', label="Points de contrôle")
+plt.title("Courbe B-spline")
+plt.legend()
+
+plt.tight_layout()
 plt.show()
